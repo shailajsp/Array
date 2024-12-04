a6f2764d58bf4954b22492775a13f011a3497a8a
shailajsp <sailajap.it@gmail.com>
2024-12-04 09:24:17 -0600

    commit

---
+++ src/PA1.java
@@ -0,0 +1,19 @@
+
+public class PA1 {
+    public static void main(String[] args) {
+
+        int[] numbers = new int[3];
+
+
+
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
